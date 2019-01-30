046c23a28a9e09dcc174920d2c08f0cca6c2daee
LeeVanie <942629585@qq.com>
2019-01-30 15:19:37 +0800

    first commit

---
+++ SocketChatDemo/lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,17 @@
+package com.example.lib;
+
+/**
+ * @类名: ${type_name}
+ * @功能描述:
+ * @作者: ${user}
+ * @时间: ${date}
+ * @最后修改者:
+ * @最后修改内容:
+ */
+public class MyClass {
+
+    public static void main(String[]args){
+        // 开启服务器
+        ClientManager.startServer();
+    }
+}
